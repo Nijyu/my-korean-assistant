85a876fe9ece208ee76d188a7eb07b8e07beb0b9
“Nijyu” <nijyudesign@gmail.com>
2024-06-24 15:44:07 +0200

    Initial commit

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './components/Home';
 import Chat from './components/Chat';
+console.log('OpenAI API Key:', process.env.REACT_APP_OPENAI_API_KEY);
 
 function App() {
   return (
